2c37720a38cac8e432e70107b0cbdda5eb2db915
CodeKaito <jerome.decinco@mawell.mi.it>
2023-09-28 12:14:12 +0200

    ✅ Upload to Production

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -5,6 +5,7 @@ import PortableText from "react-portable-text";
 import { useForm, SubmitHandler } from "react-hook-form";
 import Head from "next/head";
 import { useState } from "react";
+import { Header } from "../../components";
 
 interface IFormInput {
   _id: string;
@@ -44,62 +45,50 @@ function Post({ post }: Props) {
   };
 
   return (
-    <>
-      <Head>
-        <title>{post.title}</title>
-      </Head>
-      <main>
-        <img
-          className="w-full h-40 object-cover"
-          src={urlFor(post.mainImage).url()}
-          alt=""
-        />
+    <main>
+      <Header />
 
-        <article className="max-w-3xl mx-auto p-5">
-          <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
-          <h2 className="text-xl font-light text-gray-500 mb-2">
-            {post.description}
-          </h2>
+      <img className="w-full h-40 object-contain" src={urlFor(post.mainImage).url()!} alt='' />
+    
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className='text-3xl mt-10 mb-3'>{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
 
-          <div className="flex items-center space-x-2">
-            <img
-              className="h-10 w-10 rounded-full object-cover"
-              src={urlFor(post.author.image).url()!}
-              alt=""
-            />
-            <p className="font-extralight text-sm">
-              Blog post by
-              <span className="text-green-600">{post.author.name}</span>-
-              Published at {new Date(post._createdAt).toLocaleString()}{" "}
-            </p>
-          </div>
+        <div className="flex items-center space-x-2">
+          <img className="h-10 w-10 rounded-full " src={urlFor(post.author.image).url()!} alt="" />
+        
+          <p className="font-extralight text-sm pl-2">Blog post by <span className="text-green-600">{post.author.name}</span> - Published on {new Date(post._createdAt).toLocaleString()}{" "} </p>
+        </div>
 
-          <div className="mt-10">
-            <PortableText
-              dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
-              projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
-              content={post.body}
-              serializers={{
+        <div className="mt-10">
+          <PortableText 
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_PROJECT_ID!}
+            content={post.body}
+            serializers={
+              {
                 h1: (props: any) => (
                   <h1 className="text-2xl font-bold my-5" {...props} />
                 ),
                 h2: (props: any) => (
                   <h2 className="text-xl font-bold my-5" {...props} />
                 ),
-                li: ({ children }: any) => (
+                h3: (props: any) => (
+                  <h3 className="text-xl font-bold my-5" {...props} />
+                ),
+                li: ({children}: any) => (
                   <li className="ml-4 list-disc">{children}</li>
                 ),
-                link: ({ href, children }: any) => (
-                  <a className="text-blue-500 hover:underline" href={href}>
-                    {children}{" "}
-                  </a>
+                link: ({children, href}: any) => (
+                  <a href={href} className="text-blue-500 hover:underline">{children}</a>
                 ),
-              }}
-            />
-          </div>
-        </article>
+              }
+            }
+          />
+        </div>
+      </article>
 
-        <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
 
         {submitted ? (
           <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto rounded">
@@ -129,7 +118,7 @@ function Post({ post }: Props) {
               <input
                 {...register("name", { required: true })}
                 className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-                placeholder="John Appleseed"
+                placeholder="Your name"
                 type="text"
               />
             </label>
@@ -138,7 +127,7 @@ function Post({ post }: Props) {
               <input
                 {...register("email", { required: true })}
                 className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-                placeholder="John Appleseed"
+                placeholder="Your email"
                 type="email"
               />
             </label>
@@ -147,7 +136,7 @@ function Post({ post }: Props) {
               <textarea
                 {...register("comment", { required: true })}
                 className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-                placeholder="John Appleseed"
+                placeholder="Comment here..."
                 rows={8}
               />
             </label>
@@ -174,7 +163,7 @@ function Post({ post }: Props) {
 
             <input
               className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline 
-            hover:cursor-pointer focus:outline-none text-white font-bold py-2 px-4 rounded"
+              hover:cursor-pointer focus:outline-none text-white font-bold py-2 px-4 rounded"
               type="submit"
               name=""
               id=""
@@ -197,8 +186,7 @@ function Post({ post }: Props) {
             </div>
           ))}
         </div>
-      </main>
-    </>
+    </main>
   );
 }
 
